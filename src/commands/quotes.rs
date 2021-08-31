634cfd4e417c6728e8d723eaa991dd261d441778
Mariii-23 <marianadinis8@hotmail.com>
2021-08-31 16:15:11 +0100

    add some quotes commands (show,add)

--- src/commands/quotes.rs
+++ src/commands/quotes.rs
@@ -12,119 +12,177 @@ use serenity::{
 use crate::modules::quotes_struct;
 use quotes_struct::*;
 
-use crate::modules::function_aux::get_name_user_by_id;
-use rand::Rng;
-
 #[group]
 #[help_available]
 #[commands(add,show,me)]
 // #[description = "\n"]
 // #[default_command(diamond)]
-#[prefixes("quotes")]
+#[prefixes("quotes","quote")]
 struct Quotes;
 
 #[command]
-#[sub_commands(add_phrase)]
+#[sub_commands(add_members,add_profs,add_general)]
 //TODO alguns parametros nao estao corretos
-async fn add(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
-
-    // let phrase = &msg.message_reference
-    let phrase = args.single_quoted::<String>()?;
-
-    // let args = args;
-
-    let mut all_quotes = AllQuotes::json_to_vec_movies(&msg);
-    let quote = Quote::build(
-        CATEGORY::MEMBERS,
-        msg.id.to_string(),
-        msg.author.id.to_string(),
-        "ola".to_string(),
-        phrase );
-
-    // let all_quotes= AllQuotes::from(quote);
-    all_quotes.add(quote);
-
-    all_quotes.quotes_to_json(msg);
-
-    msg.reply(ctx,"Quote add\n").await?;
-    Ok(())
+async fn add(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
+    add_members(ctx, msg, args).await
 }
 
 #[command]
-#[aliases(phrase)]
-async fn add_phrase(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
-
+#[max_args(2)]
+#[min_args(1)]
+#[aliases(members)]
+async fn add_members(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
     let phrase = args.single_quoted::<String>()?;
-    let nick =
-        match msg.author.nick_in(&ctx, msg.guild_id.unwrap()).await {
-        None => String::from(&msg.author.name),
-        Some(name) => name,
+    let mut all_quotes = AllQuotes::json_to_vec_movies(msg);
+    let quote = if msg.mentions.is_empty() {
+        let nick = match msg.author.nick_in(&ctx, msg.guild_id.unwrap()).await {
+                None => String::from(&msg.author.name),
+                Some(name) => name,
+        };
+
+        Quote::build(
+            CATEGORY::MEMBERS,
+            msg.id.to_string(),
+            msg.author.id.to_string(),
+            nick,
+            phrase
+        )
+    } else {
+        let person = &msg.mentions[0];
+        let nick = match &person.nick_in(&ctx, msg.guild_id.unwrap()).await {
+            None => String::from(&person.name),
+            Some(name) => String::from(name),
+        };
+
+        Quote::build(
+            CATEGORY::MEMBERS,
+            msg.id.to_string(),
+            person.id.to_string(),
+            nick,
+            phrase
+        )
     };
+
+    all_quotes.add(quote);
+    all_quotes.quotes_to_json(msg);
+    msg.reply(ctx,"Quote added\n").await?;
+    Ok(())
+}
+
+
+#[command]
+#[max_args(2)]
+#[min_args(2)]
+#[aliases(profs)]
+async fn add_profs(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+    let phrase = args.single_quoted::<String>()?;
+    let nick = args.single_quoted::<String>()?;
 
     let mut all_quotes = AllQuotes::json_to_vec_movies(msg);
     let quote = Quote::build(
-        CATEGORY::MEMBERS,
+        CATEGORY::PROFS,
         msg.id.to_string(),
-        msg.author.id.to_string(),
+        nick.to_lowercase(),
         nick,
         phrase );
 
     all_quotes.add(quote);
     all_quotes.quotes_to_json(msg);
-    msg.reply(ctx,"Quote add\n").await?;
+    msg.reply(ctx,"Quote added\n").await?;
     Ok(())
 }
-
 
 #[command]
-async fn show(ctx: &Context, msg: &Message) -> CommandResult {
-    let quotes = AllQuotes::json_to_vec_movies(&msg);
+#[max_args(2)]
+#[min_args(2)]
+#[aliases(general)]
+async fn add_general(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+    let phrase = args.single_quoted::<String>()?;
+    let user_id = args.single_quoted::<String>()?;
 
-    // let my_quotes = quotes.get_by_user_id(msg.author.id.to_string());
+    let mut all_quotes = AllQuotes::json_to_vec_movies(msg);
+    let quote = Quote::build(
+        CATEGORY::GENERAL,
+        msg.id.to_string(),
+        String::from(&user_id),
+        user_id,
+        phrase );
 
-    match quotes.quotes {
-        None => (),
-        Some(map_category) => {
-            for map_id in map_category.values() {
-                for vec_quotes in map_id.values() {
-                    for quote in vec_quotes {
-                        println!("{:?}",quote);
-                    }
-                }
-            }
-        }
-    }
-
+    all_quotes.add(quote);
+    all_quotes.quotes_to_json(msg);
+    msg.reply(ctx,"Quote added\n").await?;
     Ok(())
 }
+
+
+/* show quotes */
+
+/* fnction debug */
+#[command]
+#[sub_commands(show_general,show_profs,show_members)]
+async fn show(ctx: &Context, msg: &Message) -> CommandResult {
+    let quotes = AllQuotes::json_to_vec_movies(msg);
+    let phrase = quotes.get_one_quote_to_string(ctx, msg).await;
+    msg.reply(ctx, phrase).await?;
+    Ok(())
+}
+
 
 #[command]
 async fn me(ctx: &Context, msg: &Message) -> CommandResult {
-    let quotes = AllQuotes::json_to_vec_movies(&msg);
-    println!("user_id: {}",msg.author.id.to_string());
-
-    let my_quotes = quotes.get_by_user_id(msg.author.id.to_string());
-    let mut phrase = String::from("No quotes found");
-
-    match my_quotes {
-        None => (),
-        Some(vetor) => {
-            let len = vetor.len();
-
-            let quote = vetor.get(rand::thread_rng().gen_range(0,len));
-            match quote {
-                None => (),
-                Some(quote) => {
-
-                    // println!("{}",&quote.quote);
-                    let name = get_name_user_by_id(msg,ctx,&quote.user_id).await;
-                    phrase = format!("\"{}\" - {} ({})",quote.quote,quote.nick,name);
-                }
-            }
-        }
-    }
-
+    let quotes = AllQuotes::json_to_vec_movies(msg);
+    let phrase = quotes.get_one_quote_by_user_id_to_string(ctx, msg,msg.author.id.to_string(), CATEGORY::MEMBERS).await;
     msg.reply(ctx, phrase).await?;
-
     Ok(())
+}
+
+#[command]
+#[max_args(1)]
+#[aliases(general)]
+async fn show_general(ctx: &Context, msg: &Message,mut args: Args) -> CommandResult {
+    let quotes = AllQuotes::json_to_vec_movies(msg);
+    let mut phrase = String::from("No quotes found");
+    if args.is_empty() {
+        phrase = quotes.get_one_quote_by_category_to_string(ctx, msg,CATEGORY::GENERAL).await;
+    } else {
+        //TODO pode se mudar para ser sem ""
+        let id = args.single_quoted::<String>()?;
+        phrase = quotes.get_one_quote_by_user_id_to_string(ctx, msg,id, CATEGORY::GENERAL).await;
+    }
+    msg.reply(ctx, phrase).await?;
+    Ok(())
+}
+
+#[command]
+#[max_args(1)]
+#[aliases(profs)]
+async fn show_profs(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+    let quotes = AllQuotes::json_to_vec_movies(msg);
+    let mut phrase = String::from("No quotes found");
+    if args.is_empty() {
+        phrase = quotes.get_one_quote_by_category_to_string(ctx, msg,CATEGORY::PROFS).await;
+    } else {
+        //TODO pode se mudar para ser sem ""
+        let id = args.single_quoted::<String>()?;
+        phrase = quotes.get_one_quote_by_user_id_to_string(ctx, msg,id, CATEGORY::PROFS).await;
+    }
+    msg.reply(ctx, phrase).await?;
+    Ok(())
+}
+
+#[command]
+#[max_args(1)]
+#[aliases(members)]
+async fn show_members(ctx: &Context, msg: &Message) -> CommandResult {
+    let person = &msg.mentions;
+    let quotes = AllQuotes::json_to_vec_movies(msg);
+    let mut phrase = String::from("No quotes found");
+    if person.is_empty() {
+        phrase = quotes.get_one_quote_by_category_to_string(ctx, msg,CATEGORY::MEMBERS).await;
+    } else {
+        let id = person[0].id.to_string();
+        phrase = quotes.get_one_quote_by_user_id_to_string(ctx, msg,id, CATEGORY::MEMBERS).await;
+    }
+    msg.reply(ctx, phrase).await?;
+   Ok(())
 }
--- src/modules/quotes_struct.rs
+++ src/modules/quotes_struct.rs
@@ -1,13 +1,16 @@
 extern crate serenity;
-use serenity::model::channel::Message;
+use serenity::{model::channel::Message, prelude::Context};
 
 extern crate serde_json;
 use serde::{Deserialize, Serialize};
 
 use crate::constantes::{EXTENSION_PATH, QUOTES_PATH};
+use crate::modules::function_aux::get_name_user_by_id;
 
 use std::fs::{write, File};
 use std::io::BufReader;
+
+use rand::Rng;
 
 #[derive(Eq, PartialEq, Serialize, Deserialize, Debug, Hash, Clone, Copy)]
 pub enum CATEGORY {
@@ -60,86 +63,91 @@ impl Quote {
 // }
 use std::collections::HashMap;
 
+// #[derive(Serialize, Deserialize, Debug)]
+// pub struct AllQuotes {
+//     // maybe alterar o hahMap<Categoru,...
+//     // para um array visto cada category ter um numero distinto
+//     pub quotes: Option<HashMap<CATEGORY, HashMap<String, Vec<Quote>>>>,
+// }
+
 #[derive(Serialize, Deserialize, Debug)]
 pub struct AllQuotes {
-    // maybe alterar o hahMap<Categoru,...
-    // para um array visto cada category ter um numero distinto
-    pub quotes: Option<HashMap<CATEGORY, HashMap<String, Vec<Quote>>>>,
+    pub members: Option<HashMap<String, Vec<Quote>>>,
+    pub general: Option<Vec<Quote>>,
+    pub profs: Option<HashMap<String, Vec<Quote>>>,
 }
 
 impl AllQuotes {
     pub fn new() -> AllQuotes {
-        AllQuotes { quotes: None }
+        AllQuotes {
+            members: None,
+            general: None,
+            profs: None,
+        }
     }
 
-    pub fn from(quote: Quote) -> AllQuotes {
-        // let mut§quotes me new = AllQuotes::new();
-        // new.add(quote);
-        // new
+    /*----------------- Add quotes --------------------------*/
+    fn add_members(&mut self, quote: Quote) {
         let id = String::from(&quote.user_id);
-        let category = &quote.category.clone();
-        let vec_quote = vec![quote];
-        let mut map_id = HashMap::new();
-        map_id.insert(id, vec_quote);
-        let mut map_category = HashMap::new();
-        map_category.insert(*category, map_id);
-        AllQuotes {
-            quotes: Some(map_category),
+
+        match &mut self.members {
+            Some(map_category) => match map_category.get_mut(&id) {
+                Some(vec_quotes) => {
+                    vec_quotes.push(quote);
+                }
+                None => {
+                    let vec_quote = vec![quote];
+                    map_category.insert(id, vec_quote);
+                }
+            },
+            None => {
+                let vec_quote = vec![quote];
+                let mut map_id = HashMap::new();
+                map_id.insert(id, vec_quote);
+                self.members = Some(map_id);
+            }
+        };
+    }
+
+    fn add_profs(&mut self, quote: Quote) {
+        let id = String::from(&quote.user_id);
+
+        match &mut self.profs {
+            Some(map_category) => match map_category.get_mut(&id) {
+                Some(vec_quotes) => {
+                    vec_quotes.push(quote);
+                }
+                None => {
+                    let vec_quote = vec![quote];
+                    map_category.insert(id, vec_quote);
+                }
+            },
+            None => {
+                let vec_quote = vec![quote];
+                let mut map_id = HashMap::new();
+                map_id.insert(id, vec_quote);
+                self.profs = Some(map_id);
+            }
+        };
+    }
+
+    fn add_general(&mut self, quote: Quote) {
+        match &mut self.general {
+            None => self.general = Some(vec![quote]),
+            Some(quotes) => quotes.push(quote),
         }
     }
 
     pub fn add(&mut self, quote: Quote) {
-        let id = String::from(&quote.id);
-        let category = &quote.category.clone();
-
-        match &mut self.quotes {
-            Some(quotes) => match quotes.get_mut(category) {
-                Some(map_category) => match map_category.get_mut(&id) {
-                    Some(vec_quotes) => {
-                        vec_quotes.push(quote);
-                    }
-                    None => {
-                        let vec_quote = vec![quote];
-                        map_category.insert(id, vec_quote);
-                    }
-                },
-                None => {
-                    let vec_quote = vec![quote];
-                    let mut map_id = HashMap::new();
-                    map_id.insert(id, vec_quote);
-                    quotes.insert(*category, map_id);
-                }
-            },
-            None => {
-                *self = AllQuotes::from(quote);
-            }
-        }
+        match quote.category {
+            CATEGORY::MEMBERS => self.add_members(quote),
+            CATEGORY::PROFS => self.add_profs(quote),
+            CATEGORY::GENERAL => self.add_general(quote),
+            // _ => (),
+        };
     }
 
-    /** Verifica se uma dada quote já se encontra guardada
-     * Ele apenas verifica na categoria da quote dada e do seu user_id,
-     * nao sei se o melhor seria ir ver a todas as frases de quotes
-     * (isso seria ao gosto dos utilizadores)*/
-    pub fn eq_quote(&self, other: &Quote) -> bool {
-        match &self.quotes {
-            None => (),
-            Some(map_category) => match map_category.get(&other.category) {
-                None => (),
-                Some(map_id) => match map_id.get(&other.user_id) {
-                    None => (),
-                    Some(quotes) => {
-                        for quote in quotes {
-                            if quote.quote.to_lowercase().eq(&other.quote.to_lowercase()) {
-                                return true;
-                            }
-                        }
-                    }
-                },
-            },
-        }
-        false
-    }
-
+    /* ----------- remove ------------- */
     //TODO Fazer : remover por id de quote
     // pub fn remove_by_id(&mut self, id: String) -> bool {
     //     let mut fail = false;
@@ -161,43 +169,143 @@ impl AllQuotes {
     //     fail
     // }
 
-    pub fn get_by_id(&self, id: String) -> Option<Vec<&Quote>> {
-        let mut quotes: Vec<&Quote> = Vec::new();
-        match &self.quotes {
+    /*----------------- Get quotes by user id --------------------------*/
+    fn get_in_members_by_user_id(&self, id: String) -> Option<Vec<&Quote>> {
+        match &self.members {
             None => (),
-            Some(map_category) => {
-                for map_id in map_category.values() {
-                    for vec_quotes in map_id.values() {
-                        for quote in vec_quotes {
-                            if quote.id == id {
-                                quotes.push(quote);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        if quotes.is_empty() {
-            None
-        } else {
-            Some(quotes)
-        }
-    }
-
-    pub fn get_by_user_id(&self, id: String) -> Option<&Vec<Quote>> {
-        match &self.quotes {
-            None => (),
-            Some(map_category) => match map_category.get(&CATEGORY::MEMBERS) {
+            Some(map_id) => match map_id.get(&id) {
                 None => (),
-                Some(map_id) => match map_id.get(&id) {
-                    None => (),
-                    Some(quotes) => return Some(quotes),
-                },
+                Some(quotes) =>{
+                    let mut  vec = Vec::new();
+                    for quote in quotes {
+                      vec.push(quote)  ;
+                    };
+                    return Some(vec)
+                }
             },
         }
         None
     }
+
+    fn get_in_profs_by_user_id(&self, id: String) -> Option<Vec<&Quote>> {
+        match &self.profs {
+            None => (),
+            Some(map_id) => match map_id.get(&id) {
+                None => (),
+                Some(quotes) =>{
+                    let mut  vec = Vec::new();
+                    for quote in quotes {
+                      vec.push(quote)  ;
+                    };
+                    return Some(vec)
+                }
+            },
+        }
+        None
+    }
+
+    fn get_in_general_by_user_id(&self, id: String) -> Option<Vec<&Quote>> {
+        match &self.general {
+            None => (),
+            Some(vec_quotes) => {
+                let mut vec = Vec::new();
+                for quote in vec_quotes {
+                    if quote.user_id.eq(&id) {
+                        vec.push(quote);
+                    }
+                }
+                if !vec.is_empty() {
+                    return Some(vec);
+                }
+            }
+        }
+        None
+    }
+
+    pub fn get_by_user_id(&self, id: String, category: CATEGORY) -> Option<Vec<&Quote>> {
+        match category {
+            CATEGORY::MEMBERS => self.get_in_members_by_user_id(id),
+            CATEGORY::GENERAL => self.get_in_general_by_user_id(id),
+            CATEGORY::PROFS => self.get_in_profs_by_user_id(id),
+        }
+    }
+
+    /*----------------- Get quotes by user id --------------------------*/
+
+    /*----------------- Get all quotes --------------------------*/
+    fn get_all_members_quotes(&self) -> Option<Vec<&Quote>> {
+        match &self.members {
+            None => None,
+            Some(map_id) => {
+                let mut vec = Vec::new();
+                for all_quotes in map_id.values() {
+                    vec.extend(all_quotes);
+                }
+                Some(vec)
+            }
+        }
+    }
+
+
+    fn get_all_profs_quotes(&self) -> Option<Vec<&Quote>> {
+        match &self.profs {
+            None => None,
+            Some(map_id) => {
+                let mut vec = Vec::new();
+                for all_quotes in map_id.values() {
+                    vec.extend(all_quotes);
+                }
+                Some(vec)
+            }
+        }
+    }
+
+    fn get_all_general_quotes(&self) -> Option<Vec<&Quote>> {
+        match &self.general {
+            None => None,
+            Some(quotes) => {
+                let mut vec = Vec::new();
+                for quote in quotes{
+                    vec.push(quote);
+                }
+                Some(vec)
+            }
+        }
+    }
+
+    pub fn get_all_quote_by_category(&self,category: CATEGORY) -> Option<Vec<&Quote>> {
+        match category {
+            CATEGORY::MEMBERS => self.get_all_members_quotes(),
+            CATEGORY::GENERAL => self.get_all_general_quotes(),
+            CATEGORY::PROFS => self.get_all_profs_quotes(),
+        }
+    }
+
+    /*---------- get ONE quote by category ------*/
+    /* get one quote by user id */
+    pub async fn get_one_quote_by_user_id_to_string(&self, ctx: &Context, msg: &Message,id: String, category: CATEGORY) -> String {
+        let quotes= self.get_by_user_id(id, category);
+        get_one_quote_to_string(ctx, msg, quotes).await
+    }
+
+    /* get one quote by category */
+    pub async fn get_one_quote_by_category_to_string(&self, ctx: &Context, msg: &Message,category: CATEGORY) -> String {
+          let quotes = self.get_all_quote_by_category(category);
+          get_one_quote_to_string(ctx, msg, quotes).await
+    }
+
+    pub async fn get_one_quote_to_string(&self, ctx: &Context, msg: &Message) -> String {
+        let number = rand::thread_rng().gen_range(0,3);
+        let quotes = match number {
+            0 => self.get_all_quote_by_category(CATEGORY::MEMBERS),
+            1 => self.get_all_quote_by_category(CATEGORY::GENERAL),
+            2 => self.get_all_quote_by_category(CATEGORY::PROFS),
+            _ => None,
+        };
+        get_one_quote_to_string(ctx, msg, quotes).await
+    }
+
+    /*----------------- Struct «--» json --------------------------*/
 
     pub fn quotes_to_json(&self, msg: &Message) {
         let mut path = String::from(QUOTES_PATH);
@@ -233,3 +341,23 @@ impl AllQuotes {
         quotes
     }
 }
+
+/* function aux */
+async fn get_one_quote_to_string(ctx: &Context, msg: &Message, vec: Option<Vec<&Quote>>) -> String {
+    match vec {
+        None => String::from("No quotes found"),
+        Some(vec) => {
+            let len = vec.len();
+            let quote = vec.get(rand::thread_rng().gen_range(0, len));
+            match quote {
+                None => String::from("No quotes found"),
+                Some(quote) => {
+                    // println!("{}",&quote.quote);
+                    let name = get_name_user_by_id(msg, ctx, &quote.user_id).await;
+                    format!("\"{}\" - {} ({})", quote.quote, quote.nick, name)
+                }
+            }
+        }
+    }
+}
+
